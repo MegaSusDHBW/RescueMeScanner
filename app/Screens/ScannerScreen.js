e2e8dffb99cd6750ccf4f0c31a6fd63830337077
SimonMeltzer-Git <i19033@hb.dhbw-stuttgart.de>
2022-05-22 22:00:37 +0200

    added decoding from Data @LukasSieber

--- app/Screens/ScannerScreen.js
+++ app/Screens/ScannerScreen.js
@@ -1,12 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
+import {Text,View,Button} from 'native-base'
 import StackNavigator from '../components/StackNavigator';
 
 export default function ScannerScreen({navigation}) {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
   const [data, setData] = useState([])
+  const [email,setEmail] = useState(null)
+  const [firstName,setFirstName] = useState(null)
+  const [lastName,setLastName]=useState(null)
+  const [organDonorState,setOrganDonorState] = useState(null)
+  const [bloodGroup,setBloodGroup] = useState(null)
+  const [emergencyEmail,setEmergencyEmail] =useState(null)
+  const [emergencyFirstName,setEmergencyFirstName] = useState(null)
+  const [emergencyLastName,setEmergencyLastName] = useState(null)
+  const [emergencyBirthDate,setEmergencyBirthDate] = useState(null)
+  const [emergencyPhone,setEmergencyPhone] = useState(null)
+
+
 
   useEffect(() => {
     (async () => {
@@ -16,14 +29,14 @@ export default function ScannerScreen({navigation}) {
   }, []);
 
   const handleBarCodeScanned = ({ type, data }) => {
-    setScanned(true);
+    
     const encodedData ={
       'input': data
     }
 
     decodedData = decodeData(encodedData)
     setData(data)
-    
+    setScanned(true);
     //alert(`Bar code with type ${type} and data ${data} has been scanned!`);
   };
 async function decodeData(data){
@@ -37,8 +50,23 @@ async function decodeData(data){
     console.log(JSON.stringify(data));
 
    const response= await fetch('http://10.0.2.2:5000/read-qrcode',requestOptions)
-   data= await response.json()
-   return data
+   responseData= await response.json()
+
+   let test = responseData.email
+
+   console.log('response '+responseData);
+   setEmail(responseData.email)
+   console.log(email);
+   setFirstName(responseData.firstname)
+   setLastName(responseData.lastname)
+   setOrganDonorState(responseData.organDonorState)
+   setBloodGroup(responseData.bloodGroup)
+   setEmergencyEmail(responseData.emergencyEmail)
+   setEmergencyFirstName(responseData.emergencyFirstname)
+   setEmergencyLastName(responseData.emergencyLastname)
+   setEmergencyBirthDate(responseData.emergencyBirthday)
+   setEmergencyPhone(responseData.emergencyPhone)
+   
 
   }
 
@@ -51,7 +79,8 @@ async function decodeData(data){
   if(scanned){
   return(
 <View>
-  <Text>{data}</Text>
+  <Text>{email}</Text>
+  
 </View>
   );  
   }
