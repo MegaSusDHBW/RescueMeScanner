84e79fb78e9351515bb77aaac0057d83a2da14f4
Nico <nico.riedlinger@gmx.de>
2022-05-22 23:29:52 +0200

    Auto stash before merge of "main" and "origin/main"

--- app/Screens/ScannerScreen.js
+++ app/Screens/ScannerScreen.js
@@ -1,23 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
-import {Text,View,Button} from 'native-base'
+import { Text, View, Button } from 'native-base'
 import StackNavigator from '../components/StackNavigator';
 
-export default function ScannerScreen({navigation}) {
+export default function ScannerScreen({ navigation }) {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
   const [data, setData] = useState([])
-  const [email,setEmail] = useState(null)
-  const [firstName,setFirstName] = useState(null)
-  const [lastName,setLastName]=useState(null)
-  const [organDonorState,setOrganDonorState] = useState(null)
-  const [bloodGroup,setBloodGroup] = useState(null)
-  const [emergencyEmail,setEmergencyEmail] =useState(null)
-  const [emergencyFirstName,setEmergencyFirstName] = useState(null)
-  const [emergencyLastName,setEmergencyLastName] = useState(null)
-  const [emergencyBirthDate,setEmergencyBirthDate] = useState(null)
-  const [emergencyPhone,setEmergencyPhone] = useState(null)
+  const [email, setEmail] = useState(null)
+  const [firstName, setFirstName] = useState(null)
+  const [lastName, setLastName] = useState(null)
+  const [organDonorState, setOrganDonorState] = useState(null)
+  const [bloodGroup, setBloodGroup] = useState(null)
+  const [emergencyEmail, setEmergencyEmail] = useState(null)
+  const [emergencyFirstName, setEmergencyFirstName] = useState(null)
+  const [emergencyLastName, setEmergencyLastName] = useState(null)
+  const [emergencyBirthDate, setEmergencyBirthDate] = useState(null)
+  const [emergencyPhone, setEmergencyPhone] = useState(null)
 
 
 
@@ -29,8 +29,8 @@ export default function ScannerScreen({navigation}) {
   }, []);
 
   const handleBarCodeScanned = ({ type, data }) => {
-    
-    const encodedData ={
+
+    const encodedData = {
       'input': data
     }
 
@@ -39,7 +39,7 @@ export default function ScannerScreen({navigation}) {
     setScanned(true);
     //alert(`Bar code with type ${type} and data ${data} has been scanned!`);
   };
-async function decodeData(data){
+  async function decodeData(data) {
     const requestOptions =
     {
 
@@ -49,24 +49,24 @@ async function decodeData(data){
     };
     console.log(JSON.stringify(data));
 
-   const response= await fetch('http://10.0.2.2:5000/read-qrcode',requestOptions)
-   responseData= await response.json()
+    const response = await fetch('http://10.0.2.2:5000/read-qrcode', requestOptions)
+    responseData = await response.json()
 
-   let test = responseData.email
+    let test = responseData.email
 
-   console.log('response '+responseData);
-   setEmail(responseData.email)
-   console.log(email);
-   setFirstName(responseData.firstname)
-   setLastName(responseData.lastname)
-   setOrganDonorState(responseData.organDonorState)
-   setBloodGroup(responseData.bloodGroup)
-   setEmergencyEmail(responseData.emergencyEmail)
-   setEmergencyFirstName(responseData.emergencyFirstname)
-   setEmergencyLastName(responseData.emergencyLastname)
-   setEmergencyBirthDate(responseData.emergencyBirthday)
-   setEmergencyPhone(responseData.emergencyPhone)
-   
+    console.log('response ' + responseData);
+    setEmail(responseData.email)
+    console.log(email);
+    setFirstName(responseData.firstname)
+    setLastName(responseData.lastname)
+    setOrganDonorState(responseData.organDonorState)
+    setBloodGroup(responseData.bloodGroup)
+    setEmergencyEmail(responseData.emergencyEmail)
+    setEmergencyFirstName(responseData.emergencyFirstname)
+    setEmergencyLastName(responseData.emergencyLastname)
+    setEmergencyBirthDate(responseData.emergencyBirthday)
+    setEmergencyPhone(responseData.emergencyPhone)
+
 
   }
 
@@ -96,7 +96,7 @@ async function decodeData(data){
   );  
   }
   return (
-    
+
     <View style={styles.container}>
       <BarCodeScanner
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
@@ -107,9 +107,9 @@ async function decodeData(data){
   );
 }
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      flexDirection: 'column',
-      justifyContent: 'center',
-    },
-  });
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+});
